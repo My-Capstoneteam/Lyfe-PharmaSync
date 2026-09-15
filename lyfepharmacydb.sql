ad70bc0eee088ebcb0a38e0044fe7885ccfc8f95
Quaso000 <151749191+Quaso000@users.noreply.github.com>
2026-09-16 03:29:50 +0800

    Add files via upload

--- lyfepharmacydb.sql
+++ lyfepharmacydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2026 at 02:01 PM
+-- Generation Time: Sep 15, 2026 at 09:29 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -57,7 +57,18 @@ INSERT INTO `audit_logs` (`log_id`, `user_id`, `action_type`, `description`, `ti
 (15, 2, 'Delete', 'Permanently deleted Batch: B-103.', '2026-09-04 18:41:04'),
 (16, 2, 'Smart Pricing', 'Mitigation applied: Reduced price of Sample 2 (Batch: b-123) from ₱22 to ₱18.7.', '2026-09-04 18:46:27'),
 (17, 2, 'Smart Pricing', 'Mitigation applied: Reduced price of Sample 2 (Batch: b-123) from ₱18.7 to ₱6.73.', '2026-09-04 18:47:19'),
-(18, 2, 'Smart Pricing', 'Mitigation applied: Reduced price of Sample 1 (Batch: 12345) from ₱42 to ₱21.', '2026-09-04 18:48:59');
+(18, 2, 'Smart Pricing', 'Mitigation applied: Reduced price of Sample 1 (Batch: 12345) from ₱42 to ₱21.', '2026-09-04 18:48:59'),
+(19, 1, 'Delete', 'Permanently deleted Batch: 12345.', '2026-09-15 22:34:14'),
+(20, 1, 'Bulk Import', 'CSV Import processed. New items: 8. Existing batches updated: 0.', '2026-09-15 22:51:25'),
+(21, 1, 'Delete', 'Permanently deleted Batch: TXN-000008.', '2026-09-15 23:03:42'),
+(22, 1, 'Delete', 'Permanently deleted Batch: TXN-000007.', '2026-09-15 23:03:45'),
+(23, 1, 'Delete', 'Permanently deleted Batch: TXN-000006.', '2026-09-15 23:03:47'),
+(24, 1, 'Delete', 'Permanently deleted Batch: TXN-000005.', '2026-09-15 23:03:50'),
+(25, 1, 'Delete', 'Permanently deleted Batch: TXN-000001.', '2026-09-15 23:03:52'),
+(26, 1, 'Delete', 'Permanently deleted Batch: TXN-000002.', '2026-09-15 23:03:54'),
+(27, 1, 'Delete', 'Permanently deleted Batch: TXN-000003.', '2026-09-15 23:03:57'),
+(28, 1, 'Delete', 'Permanently deleted Batch: TXN-000004.', '2026-09-15 23:03:59'),
+(29, 2, 'POS Sale', 'Stock deducted for TXN-000001. Items: Sample 2 (Generic) (x6).', '2026-09-16 02:00:31');
 
 -- --------------------------------------------------------
 
@@ -85,8 +96,7 @@ INSERT INTO `inventory_batches` (`batch_id`, `product_id`, `batch_number`, `expi
 (4, 4, 'B-104', '2028-01-12', 200, 9.00, 'Inactive'),
 (7, 7, 'B-107', '2028-11-05', 300, 5.00, 'Inactive'),
 (9, 12, 'B-103', '2026-09-14', 112, 11.00, 'Inactive'),
-(10, 13, '12345', '2026-09-14', 108, 21.00, 'Active'),
-(11, 14, 'b-123', '2026-10-10', 43, 6.73, 'Active'),
+(11, 14, 'b-123', '2026-10-10', 37, 6.73, 'Active'),
 (12, 15, 'B-201', '2027-11-03', 1498, 5.50, 'Inactive'),
 (15, 18, 'B-201', '2027-11-03', 1498, 5.50, 'Inactive'),
 (16, 19, 'B-202', '2026-05-03', 912, 8.00, 'Inactive'),
@@ -122,7 +132,6 @@ INSERT INTO `products` (`product_id`, `barcode`, `generic_name`, `brand_name`, `
 (8, 'BAR-108', 'Losartan 50mg Tab', 'Generic', 'Antihypertensive', 50, 'OTC'),
 (10, 'BAR-110', 'Mefenamic Acid 500mg', 'Generic', 'NSAID', 50, 'OTC'),
 (12, 'BAR-47570', 'Ascorbic Acid', 'Generic', 'Uncategorized', 50, 'OTC'),
-(13, 'BAR-31928', 'Sample 1', 'Generic', 'Uncategorized', 50, 'Rx'),
 (14, 'BAR-37881', 'Sample 2', 'Generic', 'Uncategorized', 50, 'OTC'),
 (15, 'BAR-58048', 'Paracetamol', 'Biogesic', 'Analgesic', 50, 'OTC'),
 (18, 'BAR-42006', 'Paracetamol', 'Biogesic', 'Analgesic', 50, 'OTC'),
@@ -167,14 +176,7 @@ CREATE TABLE `sales_items` (
 --
 
 INSERT INTO `sales_items` (`sales_item_id`, `transaction_id`, `batch_id`, `quantity_sold`, `subtotal`) VALUES
-(1, 1, 2, 1, 5.00),
-(5, 3, 1, 1, 13.00),
-(8, 4, 7, 1, 5.00),
-(9, 5, 10, 13, 273.00),
-(10, 6, 9, 10, 110.00),
-(11, 7, 15, 2, 11.00),
-(12, 7, 12, 2, 11.00),
-(13, 8, 10, 2, 42.00);
+(1, 1, 11, 6, 40.38);
 
 -- --------------------------------------------------------
 
@@ -199,14 +201,7 @@ CREATE TABLE `sales_transactions` (
 --
 
 INSERT INTO `sales_transactions` (`transaction_id`, `user_id`, `transaction_date`, `total_amount`, `total_qty`, `payment_method`, `customer_name`, `prc_license`, `ptr_number`) VALUES
-(1, 2, '2026-07-15 00:00:00', 20.00, 0, 'Cash', NULL, NULL, NULL),
-(2, 3, '2026-07-15 00:00:00', 172.00, 0, 'Cash', NULL, NULL, NULL),
-(3, 2, '2026-07-15 00:00:00', 13.00, 0, 'Cash', NULL, NULL, NULL),
-(4, 3, '2026-07-15 00:00:00', 22.00, 0, 'Cash', NULL, NULL, NULL),
-(5, 2, '2026-09-04 19:48:43', 273.00, 13, 'Cash', 'quaso', '1234567', '123456789'),
-(6, 2, '2026-09-04 19:50:12', 110.00, 10, 'Cash', '', '', ''),
-(7, 2, '2026-09-04 19:50:30', 22.00, 4, 'Cash', '', '', ''),
-(8, 2, '2026-09-04 19:52:27', 42.00, 2, 'Cash', 'asdas', 'asdad', 'dasd');
+(1, 2, '2026-09-16 02:00:31', 40.38, 6, 'Cash', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -235,8 +230,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `role_id`, `first_name`, `middle_initial`, `last_name`, `phone_number`, `email`, `username`, `password`, `status`, `last_login`, `last_active`, `sms_alerts_enabled`) VALUES
-(1, 1, 'John Kaye', '', 'Fernandez', '12345678990', 'admin', 'johnkaye123', '123', 'Offline', '2026-09-04 22:27:24', '2026-09-05 01:45:57', 1),
-(2, 2, 'Joseph', '', 'Osena', '09936178148', 'joseph_staff', 'osep123', '123', 'Offline', '2026-09-04 18:52:25', '2026-09-04 22:26:46', 1),
+(1, 1, 'John Kaye', '', 'Fernandez', '12345678990', 'admin', 'johnkaye123', '123', 'Offline', '2026-09-16 03:21:43', '2026-09-16 03:26:14', 1),
+(2, 2, 'Joseph', '', 'Osena', '09936178148', 'joseph_staff', 'osep123', '123', 'Offline', '2026-09-16 02:01:38', '2026-09-16 02:01:38', 1),
 (3, 2, 'Francis', '', 'Mariscal', '', 'francis_staff', 'francis123', 'lyfe2026', 'Offline', NULL, NULL, 1),
 (5, 2, 'quaso', 'D', 'bread', '09123456798', 'name@gmail.com', 'quaso', '123', 'Offline', NULL, NULL, 1);
 
@@ -300,19 +295,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `audit_logs`
 --
 ALTER TABLE `audit_logs`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `inventory_batches`
 --
 ALTER TABLE `inventory_batches`
-  MODIFY `batch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `batch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -324,13 +319,13 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `sales_items`
 --
 ALTER TABLE `sales_items`
-  MODIFY `sales_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `sales_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `sales_transactions`
 --
 ALTER TABLE `sales_transactions`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
